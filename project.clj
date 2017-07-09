ddc6a50f87b3f10d3cd4c32afdd476755a0f5ad8
maystrovyy <maystrovyy@gmail.com>
2017-07-09 17:32:49 +0300

    Added project.clj info

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject accent "0.0.4"
+  :description "Spring Boot Profiling helper"
+  :url "https://github.com/maystrovyy/spring-boot-profiling"
+  :author "maystrovyy"
+  :source-paths ["src"])
